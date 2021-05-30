9cbacccd6353b7979f9a9d00e3c3948fb70eeafb
YusukeOgata20 <85038686+YusukeOgata20@users.noreply.github.com>
2021-05-30 22:50:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,12 +3,13 @@ from discord.ext import tasks
 import os
 import traceback
 import discord
+from datetime import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = os.environ['DISCORD_POST_CHANNEL']
 client = discord.Client()
-
+channel_sent = None
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -16,19 +17,24 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
+"""
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
+"""
 
-# 60秒に一回ループ
 @tasks.loop(seconds=60)
-async def loop():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')  
-
-#ループ処理実行
-loop.start()
+async def timeloop():
+    now = datetime.now().strftime('%H:%M')
+    if now == '22:51':
+        await channel_sent.send(f"今日の予定\n{member_mention} テスト")#f文字列
+        
+@client.event
+async def on_ready():
+    global channel_sent
+    global member_mention
+    member_mention = "<@609895586979643415>"
+    channel_sent = client.get_channel(848287069217620038)
+    timeloop.start() #定期実行するメソッドの後ろに.start()をつける
 
 bot.run(token)
